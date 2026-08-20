e126185e7353953e58a81af55721c042c60fb1bc
Surabhi Dhore <surabhi.dhore24@gmail.com>
2026-08-20 10:27:31 +0530

    Assignment 2

---
+++ Assignment2.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3a822fd5-7793-4591-b4ba-f14e80e97bd8",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "a=float(input(\"Enter first number\"))\n",
+    "b=float(input(\"Enter second number\"))\n",
+    "c=float(input(\"Enter third number\"))\n",
+    "\n",
+    "if a>b and a>c:\n",
+    "    print(a, \" is the largest number\")\n",
+    "elif b>c and b>a:\n",
+    "    print(b, \" is the largest number\")\n",
+    "else:\n",
+    "    print(c, \" is the largest number\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.9"
+  },
+  "widgets": {
+   "application/vnd.jupyter.widget-state+json": {
+    "state": {},
+    "version_major": 2,
+    "version_minor": 0
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
